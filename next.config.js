aacaea56b621c62de72427b9bcfdfa4a942b2c7d
John Strunk <john.strunk@gmail.com>
2023-12-30 19:52:03 +0000

    Deploy to GitHub Pages
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+let assetPrefix = ''
+let basePath = ''
+
+// https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
+module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+    output: 'export',
+    distDir: 'out',
+}
