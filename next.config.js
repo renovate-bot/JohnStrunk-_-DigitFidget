8ecc719d0d5da21ed957855937a8b35e52d07636
John Strunk <john.strunk@gmail.com>
2023-12-30 21:10:37 +0000

    Try again to get images working
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-let assetPrefix = ''
-let basePath = ''
-
 // https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
 const isGithubActions = process.env.GITHUB_ACTIONS || false
+let basePath = ''
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
   basePath = `/${repo}`
 }
 
 
 module.exports = {
-    assetPrefix: assetPrefix,
-    basePath: basePath,
-    // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-    output: 'export',
-    distDir: 'out',
+  assetPrefix: basePath + '/',
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
+  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  output: 'export',
+  distDir: 'out',
 }
