e2212ea67b209468fea9133600d2ae69713d8889
John Strunk <john.strunk@gmail.com>
2023-12-31 01:13:32 +0000

    Run tests as a part of the GH actions
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- app/lib/board.spec.ts
+++ app/lib/board.spec.ts
@@ -2,7 +2,7 @@ import assert from "assert";
 import { Board } from "./board";
 
 describe("Board", () => {
-    it("should be created with a size", () => {
+    it("should be created with the given size", () => {
         const board = new Board(5);
         assert.strictEqual(board.size, 5);
     });
