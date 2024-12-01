61cb17a4b33d3df577973093513883d6b76d5efc
John Strunk <john.strunk@gmail.com>
2024-12-01 23:57:43 +0000

    Add router
    Signed-off-by: John Strunk <john.strunk@gmail.com>

---
+++ src/About.tsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router";
+
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the about page.</p>
+            <NavLink to="../">back</NavLink>
+        </div>
+    )
+}
