c0e8169699b2ce0df3927799f2d808e27e42fd70
John Strunk <john.strunk@gmail.com>
2025-02-22 02:02:50 +0000

    Re-organize repo structure
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
+/// <reference types="vitest/config" />
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // https://vite.dev/guide/static-deploy.html#github-pages
   base: '/DigitFidget/',
-  plugins: [react(), tailwindcss()],
+  define: {
+    // https://vitest.dev/guide/in-source.html#production-build
+    'import.meta.vitest': 'undefined',
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  test: {
+    // Enable in-source testing
+    // https://vitest.dev/guide/in-source.html#setup
+    includeSource: ['src/**/*.{js,ts,jsx,tsx}'],
+  },
 })
